7d43e74f61ca4a3bfb3da65a3913e4d249bd6ff9
Siddhapura <jay.siddhapura@cgi.com>
2020-07-28 12:44:26 +0200

    recipe list input upgraded from local reference technique to the form control technique

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, ViewChild, ElementRef, EventEmitter, Output } from '@angular/core';
 import { Ingredient } from 'src/app/shared/ingredient.model';
 import { shoppingListService } from '../shopping-list.service';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -9,24 +10,14 @@ import { shoppingListService } from '../shopping-list.service';
 })
 export class ShoppingEditComponent implements OnInit {
 
-  // viewChild for name input
-  @ViewChild('nameInput', {static:false}) 
-  nameInputRef:ElementRef;
-
-  @ViewChild('amountInput',{static:false}) 
-  amountInputRef:ElementRef; 
-
-
-
   constructor(private slService:shoppingListService) { }
 
   ngOnInit() {
   }
 
-  onAdd(){
-    const ingName = this.nameInputRef.nativeElement.value;
-    const ingAmount = this.amountInputRef.nativeElement.value;
-    const newIngredient = new Ingredient(ingName, ingAmount);
+  onAddItem(form: NgForm){
+    const value = form.value;
+    const newIngredient = new Ingredient(value.name, value.amount);
     this.slService.addIngredient(newIngredient);
   }
 
